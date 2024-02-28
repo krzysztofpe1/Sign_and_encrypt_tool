87c59505e8d3d5a6c78f33a61cb03ea0df04016a
Krzysztof Pecyna <krzysztofpe-01@o2.pl>
2024-02-28 12:27:42 +0100

    Created initial solution with windows client and class library.

---
+++ WindowsClient/WindowsClient/MainWindow.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Diagnostics;
+using System.Windows;
+
+namespace WindowsClient;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void BrowseButton_Click(object sender, RoutedEventArgs e)
+    {
+        if(sender == signatureCheckBrowseButton)
+        {
+            Debug.WriteLine("Check");
+        }
+        else if(sender == signatureSignBrowseButton)
+        {
+            Debug.WriteLine("Sign");
+        }
+    }
+}
