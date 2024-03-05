399527761daabd7d8bca664369a1470cbf2085b0
Krzysztof Pecyna <krzysztofpe-01@o2.pl>
2024-03-05 17:11:17 +0100

    Added non encrypted private key verification and static class MessageBoxes.

--- WindowsClient/WindowsClient/KeysManagementWindow.xaml.cs
+++ WindowsClient/WindowsClient/KeysManagementWindow.xaml.cs
@@ -1,10 +1,15 @@
 ﻿using Microsoft.Win32;
+using System.IO;
+using System.Security.Cryptography;
 using System.Windows;
 
 namespace WindowsClient;
 
 public partial class KeysManagementWindow : Window
 {
+    /// <summary>
+    /// Change this field value accordingly: true - correctly parsed one of the keys, false - user closed the window
+    /// </summary>
     private bool _returnValue = false;
     public KeysManagementWindow()
     {
@@ -21,26 +26,101 @@ public partial class KeysManagementWindow : Window
     {
         var ofd = new OpenFileDialog();
         ofd.Filter = "All files (*.*)|*.*";
-        
+
         if (!ofd.ShowDialog() == true)
             return;
-        SimpleInputWindow siw;
-        do
-        {
-            siw = new();
-            siw.ShowDialog();
-            if (siw.Input == -1)
-            {
-                return;
-            }
-        } while (true);
+
         if (sender == privateKeyBrowseButton)
         {
-
+            privateKeyPathTextBox.Text = ofd.FileName;
         }
-        else if(sender == publicKeyBrowseButton)
+        else if (sender == publicKeyBrowseButton)
         {
+            publicKeyPathTextBox.Text = ofd.FileName;
+        }
+    }
+
+    private void ContinueButton_Click(object sender, RoutedEventArgs e)
+    {
+        var privKeyPath = privateKeyPathTextBox.Text;
+        var pubKeyPath = publicKeyPathTextBox.Text;
+        if (privKeyPath == string.Empty || pubKeyPath == string.Empty)
+        {
+            MessageBoxes.Error("You have to specify path for one of keys.", "Invalid keys path");
+        }
+
+        if (privKeyPath != string.Empty)
+        {
+            try
+            {
+                if (!File.Exists(privKeyPath))
+                {
+                    MessageBoxes.Error("File under specified path doesn't exist.", "Invalid private key path");
+                }
+            }
+            catch
+            {
+                MessageBoxes.Error("Program doesn't have sufficient privileges to open private key file.", "Invalid private key path");
+            }
+            string privateKeyText = string.Empty;
+
+            //Private Key is encrypted
+            if (privateKeyEncryptedCheckBox.IsChecked.Value)
+            {
+
+                SimpleInputWindow siw;
+                do
+                {
+                    siw = new();
+                    siw.ShowDialog();
+                    if (siw.Input == -1)
+                    {
+                        return;
+                    }
+                    try
+                    {
+                        using (StreamReader sr = new(privKeyPath))
+                        {
+                            string encryptedText = sr.ReadToEnd();
+                            //decrypting the key
+                            string decryptedText;
+
+                            using RSA rsa = RSA.Create();
+                            rsa.ImportRSAPrivateKey(Convert.FromBase64String(encryptedText), out _);
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBoxes.Error(ex.Message, "Reading private key error");
+                    }
+                } while (true);
+            }
+            //Private Key is not encrypted
+            else
+            {
+                try
+                {
+                    using (StreamReader sr = new(privKeyPath))
+                    {
+                        string keyText = sr.ReadToEnd();
+
+                        using RSA rsa = RSA.Create();
+                        rsa.ImportRSAPrivateKey(Convert.FromBase64String(keyText), out _);
+                        _returnValue = true;
+                        Close();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBoxes.Error(ex.Message, "Reading private key error");
+                }
+            }
 
         }
+    }
+
+    private void GenerateButton_Click(object sender, RoutedEventArgs e)
+    {
+
     }
 }
---
+++ WindowsClient/WindowsClient/MessageBoxes.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+
+namespace WindowsClient;
+
+internal static class MessageBoxes
+{
+    public static void Info(string message, string caption)
+    {
+        MessageBox.Show(message, caption, MessageBoxButton.OK, MessageBoxImage.Information);
+    }
+
+    public static void Warn(string message, string caption)
+    {
+        MessageBox.Show(message, caption, MessageBoxButton.OK, MessageBoxImage.Warning);
+    }
+
+    public static void Error(string message, string caption)
+    {
+        MessageBox.Show(message, caption, MessageBoxButton.OK, MessageBoxImage.Error);
+    }
+}
